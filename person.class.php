78b4c11f990a03db21d1c5c3c399be1d16b80b57
Bastiaan Bitter <bastiaan@skybluemedia.nl>
2013-12-10 14:37:00 +0100

    Update person.class.php

--- person.class.php
+++ person.class.php
@@ -1,5 +1,36 @@
 <?php
 
-class person
+//Write a class for a 'person'
+class Person{
+	private $gender;
+	private $firstName;
+	private $lastName;
+	private $age;
+	private $legs;
+	private $arms;
+	private $hair;
+	private $eyes;
+	private $ears;
+	private $nose;
+	
+	public function __construct($gender, $firstName, $lastName, $age, $legs, $arms, $hair, $eyes, $ears, $nose){
+		$this->gender = $gender;
+		$this->firstName = $firstName;
+		$this->lastName = $lastName;
+		$this->age = $age;
+		$this->legs = $legs;
+		$this->arms = $arms;
+		$this->hair = $hair;
+		$this->eyes = $eyes;
+		$this->ears = $ears;
+		$this->nose = $nose;
+	}
+	
+	public function postGender(){
+		return $this->gender;
+	}
+}
 
+$person = new Person(26);
+echo $person->postGender();
 ?>
